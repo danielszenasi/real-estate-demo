4b79cc9abcccf0743e915cd8d381d0dcb818a37b
Daniel Szenasi <daniel.szenasi@supercharge.io>
2019-03-23 19:12:25 +0100

    feat: generate function

---
+++ src/lambda/listings-read-all.js
@@ -0,0 +1,9 @@
+import prisma from './generated/prisma-client'
+
+exports.handler = async function (event, context, callback) {
+  const body = JSON.stringify(await prisma.listings());
+  callback(null, {
+    statusCode: 200,
+    body,
+  });
+};
