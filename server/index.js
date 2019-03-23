237e1c89ec4b45624b144753511f32ee1f64f6b9
Daniel Szenasi <daniel.szenasi@supercharge.io>
2019-03-23 21:53:55 +0100

    fix: add prisma bindings

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const { prisma } = require('../src/lambda/generated/prisma-client')
+const { Prisma } = require('prisma-binding')
 const { ApolloServer, gql } = require('apollo-server-lambda')
+const { typeDefs: td } = require('../src/lambda/generated/prisma-client/prisma-schema')
 
 const typeDefs = gql`
   type Query {
@@ -15,8 +16,8 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        listings: (parent, args, context) => {
-            return context.prisma.listings()
+        listings: (parent, args, context, info) => {
+            return context.prisma.query.artists({}, info)
         },
         hello: (_, { name }) => `Hello ${name || 'world'}`
     }
@@ -25,9 +26,13 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: {
-        prisma,
-    },
+    context: req => ({
+        ...req,
+        prisma: new Prisma({
+            typeDefs: td,
+            endpoint: 'https://dsz-real-estate-0dedd42c3f.herokuapp.com/dsz-real-estate-service/dev',
+        }),
+    }),
 })
 
 exports.handler = server.createHandler()
